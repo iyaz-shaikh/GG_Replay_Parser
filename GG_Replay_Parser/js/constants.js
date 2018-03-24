bf90584a5b11ab85877ed15271e182e1e2334770
Flashnub <Flashnub>
2018-03-24 19:09:36 -0400

    Add project files.

---
+++ GG_Replay_Parser/js/constants.js
@@ -0,0 +1,42 @@
+﻿let REPLAY_LENGTH = 504;
+let REPLAY_START = 56;
+
+//Keys
+let WINNER_ID = "Winner";
+let PLAYER1_STEAMID = "Player1";
+let PLAYER2_STEAMID = "Player2";
+let UPLOADER_STEAMID = "Uploader";
+let TIMESTAMP_ID = "Timestamp";
+let CHARACTER1_ID = "Character1";
+let CHARACTER2_ID = "Character2";
+let YEAR_ID = "Year";
+let MONTH_ID = "Month";
+let DAY_ID = "Day";
+let HOUR_ID = "Hour";
+let MINUTE_ID = "Minute";
+let SECOND_ID = "Second";
+
+//offsets
+let YEAR_OFFSET = 1792;
+let STEAMID_PREFIX = "1100001";
+
+//Indices
+let WINNER_INDICES = [0, 1];
+let CHARACTER1_INDICES = [40, 41];
+let CHARACTER2_INDICES = [64, 65];
+let YEAR_INDICES = [88, 89];
+let MONTH_INDICES = [96, 97];
+let DAY_INDICES = [104, 105];
+let HOUR_INDICES = [112, 113];
+let MINUTE_INDICES = [120, 121];
+let SECOND_INDICES = [128, 129];
+
+let P1STEAMID_INDICES = [30, 31, 28, 29, 26, 27, 24, 25];
+let P2STEAMID_INDICES = [54, 55, 52, 53, 50, 51, 48, 49];
+let UPLOADERSTEAMID_INDICES = [78, 79, 76, 77, 74, 75, 72, 73];
+
+
+//Unused
+//let PLAYER1_INDICES = [24, 25, 26, 27, 28, 29, 30, 31];
+//let PLAYER2_INDICES = [48, 49, 50, 51, 52, 53, 54, 55];
+//let UPLOADER_INDICES = [72, 73, 74, 75, 76, 77, 78, 79];
---
+++ GG_Replay_Parser/js/parser.js
@@ -0,0 +1,193 @@
+﻿// Your code here!
+
+$(document).ready(function () {
+    $("#submit").click(function () {
+        onFileUpload();
+    });
+});
+
+function onFileUpload() {
+    var uploadedFile = document.getElementById("fileUpload").files[0];
+
+    if (uploadedFile != null) {
+        parseEntireReplayFile(uploadedFile);
+    }
+}
+
+function parseEntireReplayFile(file) {
+    var fReader = new FileReader();
+    var masterFileString = "";
+    fReader.addEventListener('load', function () {
+        var base10Array = new Uint8Array(this.result);
+        var stringHexArray = new Array(base10Array.length); //for debugging
+        var i = 0;
+        while (i < base10Array.length) {
+            // map to hex
+            stringHexArray[i] = (base10Array[i] < 16 ? '0' : '') + base10Array[i].toString(16);
+            masterFileString = masterFileString + stringHexArray[i];
+            i++;
+        }
+        base10Array = null; // free memory
+      
+        var results = new Array();
+        let uniqueSteamIDs = new Set();
+
+        //Begin Parsing.
+        for (var hIndex = REPLAY_START; hIndex < masterFileString.length; hIndex += REPLAY_LENGTH) {
+            if (hIndex + REPLAY_LENGTH >= masterFileString.length)
+                break;
+
+            var snippet = "";
+            for (var sIndex = 0; sIndex < REPLAY_LENGTH; sIndex++) {
+                snippet = snippet + masterFileString.charAt(hIndex + sIndex);
+            }
+            var parsedData = {}; //dictionary
+
+            parsedData[WINNER_ID] = parseSnippetWithIndices(snippet, WINNER_INDICES);
+            parsedData[UPLOADER_STEAMID] = parseSnippetWithIndices(snippet, UPLOADERSTEAMID_INDICES, true, null, STEAMID_PREFIX);
+            parsedData[PLAYER1_STEAMID] = parseSnippetWithIndices(snippet, P1STEAMID_INDICES, true, null, STEAMID_PREFIX);
+            parsedData[PLAYER2_STEAMID] = parseSnippetWithIndices(snippet, P2STEAMID_INDICES, true, null, STEAMID_PREFIX);
+            parsedData[CHARACTER1_ID] = parseSnippetWithIndices(snippet, CHARACTER1_INDICES);
+            parsedData[CHARACTER2_ID] = parseSnippetWithIndices(snippet, CHARACTER2_INDICES);
+
+            //Timestamp
+            parsedData[YEAR_ID] = parseSnippetWithIndices(snippet, YEAR_INDICES, true, YEAR_OFFSET);
+            parsedData[MONTH_ID] = parseSnippetWithIndices(snippet, MONTH_INDICES, true);
+            parsedData[DAY_ID] = parseSnippetWithIndices(snippet, DAY_INDICES, true);
+            parsedData[HOUR_ID] = parseSnippetWithIndices(snippet, HOUR_INDICES, true);
+            parsedData[MINUTE_ID] = parseSnippetWithIndices(snippet, MINUTE_INDICES, true);
+            parsedData[SECOND_ID] = parseSnippetWithIndices(snippet, SECOND_INDICES, true);
+
+            //Need to do some formatting for this
+            parsedData[TIMESTAMP_ID] = assembleTimeStamp(parsedData);
+
+            //Check for unique Steam IDs
+            if (!uniqueSteamIDs.has(parsedData[PLAYER1_STEAMID])) {
+                uniqueSteamIDs.add(parsedData[PLAYER1_STEAMID]);
+            }
+            if (!uniqueSteamIDs.has(parsedData[PLAYER2_STEAMID])) {
+                uniqueSteamIDs.add(parsedData[PLAYER2_STEAMID]);
+            }
+
+            results.push(parsedData);
+        }
+
+        //Render unique Steam Ids
+        var oldSteamIDs = $("#uniqueSteamIDs").find("tbody");
+        var newSteamIDs = $("<tbody>");
+        let uniqueSteamIDsIterator = uniqueSteamIDs.values();
+        let uniqueSteamIDsArray = []; //For Future use.
+        var uniqueSteamIDsIndex = 0;
+        for (let uniqueSteamID of uniqueSteamIDsIterator) {
+            var newRow = $("<tr>");
+            newRow.append($("<td>" + uniqueSteamID + "</td >"));
+            newRow.append($("</tr>"));
+            newSteamIDs.append(newRow);
+            uniqueSteamIDsArray[uniqueSteamIDsIndex] = uniqueSteamID;
+            uniqueSteamIDsIndex++;
+        }
+
+        newSteamIDs.append("</tbody>");
+        oldSteamIDs.replaceWith(newSteamIDs);
+
+
+        //Render stuff in table
+        var oldRows = $("#debugTable").find("tbody");
+        var newRows = $("<tbody>");
+
+        for (var rIndex = 0; rIndex < results.length; rIndex++) {
+            let replay = results[rIndex];
+            var newRow = $("<tr>");
+            newRow.append($("<td>" + replay[WINNER_ID] + "</td >"));
+            newRow.append($("<td>" + replay[UPLOADER_STEAMID] + "</td >"));
+            newRow.append($("<td>" + replay[PLAYER1_STEAMID] + "</td >"));
+            newRow.append($("<td>" + replay[CHARACTER1_ID] + "</td >"));
+            newRow.append($("<td>" + replay[CHARACTER2_ID] + "</td >"));
+            newRow.append($("<td>" + replay[PLAYER2_STEAMID] + "</td >"));
+            newRow.append($("<td>" + replay[TIMESTAMP_ID] + "</td >"));
+            newRow.append($("</tr>"));
+            newRows.append(newRow);
+        }
+        newRows.append($("</tbody>"));
+        oldRows.replaceWith(newRows);
+
+
+        //debug shit here
+        /*
+        var log = document.getElementById("log");
+        log.innerHTML = log.innerHTML + " " + masterFileString;
+        */
+    });
+    fReader.readAsArrayBuffer(file);
+
+}
+
+
+
+//helper
+//Offset can only be used in cases < 2^53
+function parseSnippetWithIndices(snippet, indices, convertToBase10, offset, prefix) {
+    var indexedSnippet = prefix != null ? prefix : "";
+    for (var i = 0; i < indices.length; i++) {
+        if (indices[i] < snippet.length) {
+            indexedSnippet = indexedSnippet + snippet.charAt(indices[i]);
+        }
+    }
+    if (convertToBase10) {
+        var base10Snippet = h2d(indexedSnippet);
+        if (offset != null) {
+            var base10SnippetInDecimal = parseInt(base10Snippet, 10);
+            base10SnippetInDecimal += offset;
+            base10Snippet = base10SnippetInDecimal.toString(10);
+        }
+        indexedSnippet = base10Snippet;
+    }
+    return indexedSnippet;
+}
+
+
+function assembleTimeStamp(result) {
+    let year = parseInt(result[YEAR_ID], 10);
+    let month = parseInt(result[MONTH_ID], 10);
+    let day = parseInt(result[DAY_ID], 10);
+    let hour = parseInt(result[HOUR_ID], 10);
+    let minute = parseInt(result[MINUTE_ID], 10);
+    let second = parseInt(result[SECOND_ID], 10);
+
+    var date = new Date(Date.UTC(year, month, day, hour, minute, second));
+    var options = {
+        weekday: "long", year: "numeric", month: "short",
+        day: "numeric", hour: "2-digit", minute: "2-digit"
+    };  
+    let dateString = date.toLocaleDateString("en-US");
+    return dateString;
+}
+
+//Online workaround to Javascript's limitation of having a max of 52 bit floating point numbers in parseInt, which causes inaccuracies when converting from hex to dec.
+
+function h2d(s) {
+
+    function add(x, y) {
+        var c = 0, r = [];
+        var x = x.split('').map(Number);
+        var y = y.split('').map(Number);
+        while (x.length || y.length) {
+            var s = (x.pop() || 0) + (y.pop() || 0) + c;
+            r.unshift(s < 10 ? s : s - 10);
+            c = s < 10 ? 0 : 1;
+        }
+        if (c) r.unshift(c);
+        return r.join('');
+    }
+
+    var dec = '0';
+    s.split('').forEach(function (chr) {
+        var n = parseInt(chr, 16);
+        for (var t = 8; t; t >>= 1) {
+            dec = add(dec, dec);
+            if (n & t) dec = add(dec, '1');
+        }
+    });
+    return dec;
+}
+
